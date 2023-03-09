6448546325c5b613fda25501204d20021b472a44
Notebook-42 <1152127784@qq.com>
2023-03-09 19:31:25 +0800

    delete

--- MyStringTest.java
+++ MyStringTest.java
@@ -573,7 +573,7 @@ class Solution {
         int i = 0;
         int j = 0;
 
-        while(i < n - m + 1) {
+        while(i < n - m + 1) {//条件什么意思？
             //找到首字母相等
             while(i < n && haystack.charAt(i) != needle.charAt(j)){
                 i++;
@@ -603,50 +603,6 @@ class Solution {
     }
 
 
-
-    /**
-     * 基于窗口滑动的算法
-     * <p>
-     * 时间复杂度：O(m*n)
-     * 空间复杂度：O(1)
-     * 注：n为haystack的长度，m为needle的长度
-     */
-    public int strStr0(String haystack, String needle) {
-        int m = needle.length();
-        // 当 needle 是空字符串时我们应当返回 0
-        if (m == 0) {
-            return 0;
-        }
-        int n = haystack.length();
-        if (n < m) {
-            return -1;
-        }
-        int i = 0;
-        int j = 0;
-        while (i < n - m + 1) {
-            // 找到首字母相等
-            while (i < n && haystack.charAt(i) != needle.charAt(j)) {
-                i++;
-            }
-            if (i == n) {// 没有首字母相等的
-                return -1;
-            }
-            // 遍历后续字符，判断是否相等
-            i++;
-            j++;
-            while (i < n && j < m && haystack.charAt(i) == needle.charAt(j)) {
-                i++;
-                j++;
-            }
-            if (j == m) {// 找到
-                return i - j;
-            } else {// 未找到
-                i -= j - 1;
-                j = 0;
-            }
-        }
-        return -1;
-    }
     public int strStr1(String haystack, String needle){
         
         return 0;
@@ -654,7 +610,6 @@ class Solution {
     
     //求next[]数组 / 前缀表
     public void getNext(int[] next,String s) {
-        System.out.println("MK-OMEN: New");
         char[] chars = s.toCharArray();
         int j = -1;
         next[0] = j;
