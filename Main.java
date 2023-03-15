a1e16076287dbd25300000354d0e03587b0513c7
Terminal-iPad <shiyu0909@outlook.com>
2023-03-15 08:07:03 +0000

    ish upload 5th

--- Main.java
+++ Main.java
@@ -98,7 +98,7 @@ public class Main
         
 	System.out.println("iPad upload");
 	System.out.println("github send 1st");
-	System.out.println("iSH upload 4th");
+	System.out.println("iSH upload 5th");
    	System.out.println("OhMyZsh Success");
     }
 
