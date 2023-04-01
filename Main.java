7eb71ef4ed0442bde730a29a01e899f29429983e
Terminal-iPad <shiyu0909@outlook.com>
2023-04-01 19:37:35 +0800

    minSubArrayLen

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
-import java.util.Arrays;
-import java.util.Scanner;
+// import java.util.Arrays;
+// import java.util.Scanner;
+import java.util.*;
 
 /**
  * 
@@ -271,6 +272,26 @@ public class Main
         }
         return result;
     }
+	
+	
+	
+	
+	//#长度最小的子数组
+	//滑动窗口法
+	public static int minSubArrayLen(int s, int[] nums) {
+		int left = 0;
+		int sum = 0;
+		int result = Integer.MAX_VALUE;//窗口长度
+		
+		for(int right = 0; right < nums.length; right++) {
+				sum += nums[right];
+				while(sum >= s) {//如果当前窗口的值大于s了，窗口就要向前移动了
+					result = Math.min(result, right - left + 1);
+					sum -= nums[left++];//sum去掉上一个值之后，left向后移动
+				}
+		}
+		return result == Integer.MAX_VALUE ? 0 : result;
+	}
 }
 
 
