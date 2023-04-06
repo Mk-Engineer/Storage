27df8ebaac0b6f1880acd81fa457a68bf556613e
Notebook-42 <1152127784@qq.com>
2023-04-07 01:56:11 +0800

    Notebook-42 Push

--- Main.java
+++ Main.java
@@ -14,8 +14,6 @@ public class Main
 {
     public static void main(String[] args) 
     {       
-		//iSH iPad
-
 		// while(true) {
         //     Scanner in = new Scanner(System.in);
         //     System.out.print("输入n: ");
@@ -621,5 +619,5 @@ public class Main
 
 
 
-    //#MK-OMEN
+    //#Notebook-42
 }
