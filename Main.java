cc95b1b16dba46dcf9fc7284502f5396e47b7b27
MK-OMEN <516730909@qq.com>
2023-04-06 22:53:03 +0800

    add fourSumCount

--- Main.java
+++ Main.java
@@ -455,6 +455,52 @@ public class Main
 
 
 
+    /**
+     * #四数之和
+     *      给定四个包含整数的数组列表 A , B , C , D ,
+     *      计算有多少个元组 (i, j, k, l) ，使得 A[i] + B[j] + C[k] + D[l] = 0。
+     * 
+     * 步骤：
+     *     首先定义 一个unordered_map，key放a和b两数之和，value 放a和b两数之和出现的次数。 
+     *     遍历大A和大B数组，统计两个数组元素之和，和出现的次数，放到map中。
+     *     定义int变量count，用来统计 a+b+c+d = 0 出现的次数。
+     *     在遍历大C和大D数组，找到如果 0-(c+d) 在map中出现过的话，
+     *     就用count把map中key对应的value也就是出现次数统计出来。
+     *     最后返回统计值 count。
+     * 
+     * @param nums1
+     * @param nums2
+     * @param nums3
+     * @param nums4
+     * @return 总元组数 \ 总次数
+     */
+    public static int fourSumCount(int[] nums1,int[] nums2,int[] nums3,int[] nums4) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int temp;
+        int res = 0;
 
-    //#
+        //统计两个数组中的元素之和，同时统计出现的次数，放入map。
+        for(int i : nums1) {
+            for(int j : nums2) {
+                temp = i + j;
+                if (map.containsKey(temp)) {
+                    map.put(temp, map.get(temp) + 1);//value + 1
+                } else {
+                    map.put(temp,1);
+                }
+            }
+        }
+
+        //统计剩余两个元素的和，在map中找到是否存在相加为0的情况，同记录次数
+        for(int i : nums3) {
+            for(int j : nums4) {
+                temp = i + j;
+                if(map.containsKey(0-temp)) {
+                    res += map.get(0-temp);
+                }
+            }
+        }
+        
+        return res;
+    }
 }
