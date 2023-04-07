a90b858cf901025b84975fa4b24bfca9c798b3c1
Notebook-42 <shiyu0909@outlook.com>
2023-04-07 16:58:53 +0800

    add fourSum whole

--- Main.java
+++ Main.java
@@ -631,7 +631,49 @@ public class Main
      * @return
      */
     public static List<List<Integer>> forSum(int[] nums, int target) {
-        
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > 0 && nums[i] > target) {
+                return result;
+            }
+
+            //去重
+            if(i > 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+
+            for(int j = i + 1; j < nums.length; j++) {
+                //对nums[j]去重
+                if(j > i + 1 && nums[j - 1] == nums[j]) {
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = nums.length - 1;
+
+                while(right > left){
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if(sum > target) {
+                        right--;
+                    } else if(sum < target) {
+                        left++;
+                    } else {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        //对nums[left]和nums[right]去重
+                        while(right > left && nums[right] == nums[right - 1])
+                            right--;
+                        while(right > left && nums[left] == nums[left + 1])
+                            left++;
+                            
+                        //双指针收缩
+                        left++;    
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
