14c0111844c006e3c8df3db6e451c198eff66add
MK-OMEN <shiyu0909@outlook.com>
2023-04-11 00:44:21 +0800

    add removeDuplicates0: MK-OMEN

--- Main.java
+++ Main.java
@@ -750,6 +750,39 @@ public class Main
         }
         return deque.isEmpty();
     }
+
+
+
+    /**
+     * #删除字符串中的所有相邻重复项
+     * 
+     * @param s
+     * @return
+     */
+    public static String removeDuplicates0(String s) {
+        //ArrayDeque会比LinkedList在除了删除元素这一点外会快一点
+        //参考：https://stackoverflow.com/questions/6163166/why-is-arraydeque-better-than-linkedlist
+
+        ArrayDeque<Character> deque = new ArrayDeque<>();
+        char ch;
+
+        for(int i = 0; i < s.length(); i++) {
+            ch = s.charAt(i);
+            if(deque.isEmpty() || deque.peek() != ch) {
+                deque.push(ch);
+            } else {
+                deque.pop();
+            }
+        }
+
+        String str = "";
+        //剩余元素即为不重复元素
+        while(!deque.isEmpty()) {
+            str = deque.pop() + str;
+        }
+
+        return str;
+    }
 }
 
 
