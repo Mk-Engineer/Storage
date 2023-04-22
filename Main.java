4ada48c55f5d07232d0e0aedaf4b3da1ddb19cd2
MK-OMEN <shiyu0909@outlook.com>
2023-04-23 05:06:38 +0800

    add topKFrequent1:MK-OMEN

--- Main.java
+++ Main.java
@@ -1040,7 +1040,42 @@ public class Main
 
 
 
-    //#
+    //#前 K 个高频元素
+    /*Comparator接口说明：
+     * 
+     * 返回负数，形参中的第一个参数排在前面；返回正数，形参中第二个参数排在前面
+     * 对于队列：排在前面意味着往队头靠
+     * 对于堆 (使用PriorityQueue实现) ：从队头到队尾按 从小到大 排就是 小顶堆
+     *                                从队头到队尾按从大到小排就是最大堆，队首元素相当于堆的根节点
+     */
+
+    //解法1：基于大顶堆实现
+    public int[] topKFrequent1(int[] nums, int k) {
+        //key为数组元素值，val为对应出现次数
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int num:nums) {
+            map.put(num,map.getOrDefault(num,0) + 1);
+        }
+
+        //在优先队列中存储二元组(num,cnt)，cnt表示元素值num在书中的出现次数
+        //出现次数按从队头到队尾的顺序是从大到小排，出现次数最多的在队头(相当于大顶堆)
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((pair1,pair2)->pair2[1]-pair1[1]);
+        
+        //大顶堆需要对所有元素进行排序
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()) {
+            pq.add(new int[]{entry.getKey(),entry.getValue()});
+        }
+
+        int[] ans = new int[k];
+        //依次从队头弹出k个，就是出现频率前k高的元素
+        for(int i = 0; i < k; i++) {
+            ans[i] = pq.poll()[0];
+        }
+        
+        return ans;
+    }
 }
 
 
