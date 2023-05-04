0771b3094f3d065170a3f647576415e1271198c5
mk-linux <shiyu0909@outlook.com>
2023-05-04 20:12:37 +0800

    Mk-linux upload Test

---
+++ main.c
@@ -0,0 +1,14 @@
+/* ************************************************************************
+> File Name:     main.c
+> Author:        程序员Carl
+> 微信公众号:    代码随想录
+> Created Time:  Thu 04 May 2023 05:24:15 PM CST
+> Description:   
+ ************************************************************************/
+#include <stdio.h>
+int main()
+{
+    printf("Hello, C language!");
+    printf("Mk-Linux Changed");
+    return 0;
+}
