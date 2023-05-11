ad29c0a7ad96b377807098bde71878364084277f
Mk-ThinkPad <shiyu0909@outlook.com>
2023-05-11 13:40:40 +0800

    Mk-ThinkPad 22/05/11

--- UploadTest.java
+++ UploadTest.java
@@ -1,6 +1,6 @@
 public class UploadTest {
     public static void main(String args[]) {
-        System.out.println("Hello Mk-ThinkPad");
         System.out.println("Hello MK-OMEN");
+        System.out.println("Mk-ThinkPad");
     }
 }
