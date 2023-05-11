76a3868552dbb36ae0de021ad700e4050b04d68b
Terminal-iPad <shiyu0909@outlook.com>
2023-05-11 14:00:59 +0800

    iSH iPad 23/05/11

--- UploadTest.java
+++ UploadTest.java
@@ -2,5 +2,6 @@ public class UploadTest {
     public static void main(String args[]) {
         System.out.println("Mk-ThinkPad");
         System.out.println("MK-OMEN");
+		System.out.prinln("iSH iPad");
     }
 }
