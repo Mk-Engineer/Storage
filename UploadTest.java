c90c83ded0b5d2732b0138897aa9af06d06aad17
mk-linux <shiyu0909@outlook.com>
2023-05-11 14:05:49 +0800

    Mk-Linux 23/05/11

--- UploadTest.java
+++ UploadTest.java
@@ -3,5 +3,6 @@ public class UploadTest {
         System.out.println("Mk-ThinkPad");
         System.out.println("MK-OMEN");
 		System.out.prinln("iSH iPad");
+        System.out.println("Mk-Linux");
     }
 }
