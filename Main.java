653170f35f40cb1967803f34fcbf95d21978d164
Terminal-iPad <shiyu0909@outlook.com>
2023-06-13 15:21:49 +0000

    T-iPad upload

--- Main.java
+++ Main.java
@@ -173,7 +173,6 @@ public class Main
         System.out.println("MMMMMMMMMMM$ZZZZZZZZZZZZZZZZNMMMMMMMMMMM");
         System.out.println("MMMMMMMMMMMMMMMZZZZZZZZZMMMMMMMMMMMMMMMM");
         System.out.println("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
-       	System.out.println("Hello Github iSH!");
 	System.out.println();
         
     }
