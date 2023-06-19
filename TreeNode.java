c69ee9f39a3efd8a07276f415cd8eeda1aa77250
Terminal-iPad <shiyu0909@outlook.com>
2023-06-19 14:30:30 +0000

    Terminal-iPad upload inorder & postorder of TreeNode.java

--- TreeNode.java
+++ TreeNode.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class TreeNode {
     int val;
     TreeNode left;
@@ -17,5 +19,84 @@ public class TreeNode {
 
     public static void main(String args[]) {
         System.out.println("Hello TreeNode iSH!");
+
+        TreeNode leftgrandson = new TreeNode(3);
+        TreeNode rightgrandson = new TreeNode(4);
+        TreeNode leftson = new TreeNode(1,leftgrandson,rightgrandson);
+        TreeNode rightson = new TreeNode(2);
+        TreeNode tree = new TreeNode(0,leftson,rightson);
+
+        Solution0 slt0 = new Solution0();
+        List result0 = slt0.preorderTraversal(tree);
+        System.out.println("PREORDER: " + Arrays.toString(result0.toArray()));
+
+        Solution1 slt1 = new Solution1();
+        List result1 = slt1.inorderTraversal(tree);
+        System.out.println("INORDER: " + Arrays.toString(result1.toArray()));
+
+        Solution2 slt2 = new Solution2();
+        List result2 = slt2.postorderTraversal(tree);
+        System.out.println("POSTORDER: " + Arrays.toString(result2.toArray()));
+    }
+}
+
+//前序遍历：中左右
+class Solution0 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        preorder(root, result);
+
+        return result;
+    }
+
+    public void preorder(TreeNode root, List<Integer> result) {
+        if(root == null) {
+            return;
+        }
+        
+        result.add(root.val);
+        preorder(root.left, result);
+        preorder(root.right,result);
+
+    }
+}
+
+//中序遍历：左中右
+class Solution1 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        inorder(root,result);
+        
+        return result;
+    }
+
+    public void inorder(TreeNode root, List<Integer> result) {
+        if(root == null) {
+            return;
+        }
+
+        inorder(root.left,result);
+        result.add(root.val);
+        inorder(root.right,result);
+    }
+}
+
+//后序遍历：左右中
+class Solution2 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        postorder(root,result);
+
+        return result;
+    }
+
+    public void postorder(TreeNode root, List<Integer> result) {
+        if(root == null) {
+            return;
+        }
+
+        postorder(root.left,result);
+        postorder(root.right,result);
+        result.add(root.val);
     }
 }
