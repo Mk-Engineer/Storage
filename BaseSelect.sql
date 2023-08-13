f452de27436ed49223050c0956c703af434e33fa
MK-OMEN <shiyu0909@outlook.com>
2023-08-13 16:37:24 +0800

    MK-OMEN update

--- BaseSelect.sql
+++ BaseSelect.sql
@@ -12,11 +12,34 @@ DCL:数据控制语言
     COMMIT \ ROLLBACK \ SAVEPOINT \ GRANT \ REVOKE
 
 */
-
-# SELECT 列 FROM 表;
-
 USE dbtest;
 
 SELECT * FROM employees;
 
 INSERT INTO employees VALUES(1003,'Jack');
+
+
+# 2. 列的别名
+# SELECT 列1 列的别名1,列2 AS 列的别名2,列3 "列的别名3" FROM 表;
+
+# 2.1 去重的情况
+# SELECT DISTINCT 列 FROM 表;
+
+
+# 3. 空值(null)参与运算
+# 结果也为 null
+
+
+# 4. 着重号 ``
+# SELECT * FROM `关键字`
+
+
+# 5. 查询常数
+# SELECT 常数,列 FROM 表
+
+
+# 6. 显示表结构
+DESCRIBE employees; /* 显示表中 字段 的 详细信息 */
+DESC employees;
+
+
