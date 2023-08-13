496e355eea299d1928943585738bd4827bb6d581
MK-OMEN <shiyu0909@outlook.com>
2023-08-13 13:48:40 +0800

    MK-OMEN create

---
+++ BaseSelect.sql
@@ -0,0 +1,20 @@
+# 基本的SELECT语句
+
+# 1.SQL的分类
+/*
+DDL:数据定义语言
+    CREATE \ ALTER \ DROP \ RENAME \ TRUNCATE 
+
+DML:数据操作语言
+    INSERT \ DELETE \ UPDATE \ SELECT
+
+DCL:数据控制语言
+    COMMIT \ ROLLBACK \ SAVEPOINT \ GRANT \ REVOKE
+
+*/
+
+USE dbtest;
+
+SELECT * FROM employees;
+
+INSERT INTO employees VALUES(1003,'Jack');
