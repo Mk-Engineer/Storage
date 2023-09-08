de66f535ee04ac481b0a97f6708ebd747ecff772
MK-OMEN <shiyu0909@outlook.com>
2023-09-08 12:53:26 +0800

    MK-OMEN update

--- SubQuery.sql
+++ SubQuery.sql
@@ -1,2 +1,25 @@
 # 26 子查询
-SELECT * FROM employees;
+/* 需求：查询谁的工资比Abel高 */
+/* 方式1：*/
+SELECT salary
+FROM employees
+WHERE last_name = 'Abel';
+
+SELECT last_name,salary
+FROM employees
+WHERE salary > 11000;
+
+/* 方式2：自连接 */
+SELECT e2.last_name,e2.salary
+FROM employees e1,employees e2
+WHERE e2.`salary` > e1.`salary` /* 多表的连接条件 */
+AND e1.last_name = 'Abel';
+
+/* 方式3：子查询 */
+SELECT last_name,salary
+FROM employees
+WHERE salary > (
+                SELECT salary
+                FROM employees
+                WHERE last_name = 'Abel'
+                );
