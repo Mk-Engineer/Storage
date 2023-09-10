c5ace50735159b6956412611188d64ab45c88651
MK-OMEN <shiyu0909@outlook.com>
2023-09-10 11:31:16 +0800

    MK-OMEN update

--- SubQuery.sql
+++ SubQuery.sql
@@ -143,4 +143,71 @@ HAVING AVG(salary) <= ALL (
 /* 注意：单行函数可以嵌套，聚合函数不可以嵌套使用 */
 
  
-                
+# 26.3 相关子查询
+/* 
+    步骤：
+        GET：从主查询中获取候选列
+        EXECUTE：子查询使用主查询的数据
+        USE：如果满足子查询的条件则返回该行
+
+    SELECT column1, column2, ..
+    FROM table1 outer                              
+    WHERE column1 operator
+                        (
+                        SELECT column1, column2
+                        FROM table2
+                        WHERE expr1 = 
+                                     outer.expr2   
+                        );
+*/       
+
+/* 题目：查询员工中工资大于本部门平均工资的员工的last_name、salary和department_id */
+/* 方式1：相关子查询 */
+SELECT last_name,salary,department_id
+FROM employees e1
+WHERE salary > (
+                SELECT AVG(salary)
+                FROM employees e2
+                WHERE department_id = e1.`department_id`   /* 与外部建立连接 */ 
+                );
+
+/* 方式2：在FROM中声明子查询 */
+SELECT department_id, AVG(salary)
+FROM employees
+GROUP BY department_id;
+
+SELECT e.last_name,e.salary,e.department_id
+FROM employees e, (
+                    SELECT department_id, AVG(salary) avg_sal
+                    FROM employees
+                    GROUP BY department_id  
+                    ) t_dept_avg_sal /* 把计算出的各部门平均工资看作一张表 */
+WHERE e.`department_id` = t_dept_avg_sal.`department_id`
+AND e.`salary` > t_dept_avg_sal.`avg_sal`;                    
+
+
+/* 题目：查询员工的id、salary按照department_name排序 */
+SELECT employee_id, salary
+FROM employees e
+ORDER BY (
+        SELECT department_name
+        FROM departments d
+        WHERE e.`department_id` = d.`department_id`    
+        ) ASC;
+
+/* 
+ * 结论:
+        除了GROUP BY 和 LIMIT 之外，其他位置都可以声明 子查询。
+
+    #sql99语法   
+        SELECT .., .., .. (存在聚合函数)                [5]
+
+        FROM .. (LEFT / RIGHT) JOIN.. ON 多表的连接条件 [1]
+        WHERE 不含聚合函数的过滤条件                     [2]
+        GROUP BY .., ..                                [3]
+        HAVING 含聚合函数的过滤条件                      [4]
+
+        ORDER BY .., ..(ASC/DESC)                      [6]
+        LIMIT .., ..                                   [7]
+
+*/        
