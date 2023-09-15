761166a5f4602f5bbb0e26c919001c52845e9ac3
MK-OMEN <shiyu0909@outlook.com>
2023-09-15 10:55:06 +0800

    MK-OMEN create

---
+++ TableCtrl.sql
@@ -0,0 +1,25 @@
+# 创建表
+CREATE TABLE IF NOT EXISTS myemp(
+id INT,
+emp_name VARCHAR(15),
+hire_date DATE
+);
+
+DESC myemp;
+
+SHOW TABLES;
+
+# 修改表
+/* 添加一个字段 */
+
+/* 修改一个字段：数据类型、长度、默认值 */
+
+/* 重命名一个字段 */
+
+/* 删除一个字段 */
+
+# 重命名表
+
+# 删除表
+
+# 清空表
