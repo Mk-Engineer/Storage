a5f2421d408886a00d616fa5eeccd4f5ff96666e
mk-linux <shiyu0909@outlook.com>
2023-12-20 18:27:02 +0800

    mk-linux update

--- ExplainTest.sql
+++ ExplainTest.sql
@@ -33,5 +33,19 @@ CREATE TABLE s2 (
     INDEX idx_key_part(key_part1,key_part2,key_part3)
 ) ENGINE=INNODB CHARSET=utf8;
 
-
+DELIMITER //
+CREATE FUNCTION rand_string1(n INT)
+    RETURNS VARCHAR(255) # 该函数返回字符串
+BEGIN
+     DECLARE chars_str VARCHAR(100) DEFAULT
+     'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRESUVWXYZ';
+     DECLARE return_str VARCHAR(255) DEFAULT '';
+     DECLARE i INT DEFAULT 0;
+     WHILE i < n DO
+        SET return_str=CONCAT(return_str,SBSTRING(chars_str,FLOOR(1+RAND()*52),1));
+        SET i = i + 1;
+     END WHILE;
+     RETURN return_str;
+END //
+DELIMITER ;
 
