d7974985ba00f1449bb4e92c0ae5e74782343740
mk-linux <shiyu0909@outlook.com>
2024-03-21 07:33:41 +0800

    linux update

--- DbAdvance.sql
+++ DbAdvance.sql
@@ -41,5 +41,70 @@ SELECT * FROM members LEFT JOIN members_detail ON members.id = members_detail.me
 
 -- 5 优化插入记录的速度
 
+CREATE TABLE IF NOT EXISTS `user` (
+ `id` INT NOT NULL AUTO_INCREMENT,
+ `name` VARCHAR(255) DEFAULT NULL,
+ `age` INT DEFAULT NULL,
+ `sex` VARCHAR(255) DEFAULT NULL,
 
+ PRIMARY KEY(`id`),
+ KEY `idx_name` (`name`) USING BTREE
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
+
+#######
+SET GLOBAL log_bin_trust_function_creators = 1;
+
+DELIMITER //
+
+CREATE FUNCTION rand_num(from_num INT, to_num INT) RETURNS INT(11)
+BEGIN
+ DECLARE i INT DEFAULT 0;
+ SET i = FLOOR(from_num +RAND()*(to_num - from_num+1));
+ RETURN i;
+END //
+
+DELIMITER ;
+
+###
+DELIMITER //
+
+CREATE PROCEDURE insert_user(max_num INT)
+BEGIN
+ DECLARE i INT DEFAULT 0;
+ SET autocommit = 0;
+ REPEAT
+  SET i = i + 1;
+  INSERT INTO `user`(name,age,sex)
+  VALUES("Atom",rand_num(1,20),"male");
+  UNTIL i = max_num
+ END REPEAT;
+ 
+ COMMIT;
+END //
+DELIMITER ;
+
+##
+#CALL insert_user(1000);
+
+SHOW INDEX FROM user;
+
+SELECT * FROM user;
+
+UPDATE user SET name = "Pluto" WHERE id = 3;
+
+
+#Analyze Table
+ANALYZE TABLE user;
+
+
+#Check Table
+CHECK  TABLE user;
+
+
+#Optimize Table
+CREATE TABLE IF NOT EXISTS t_myisam(id INT,name VARCHAR(15)) ENGINE = MYISAM;
+OPTIMIZE TABLE t_myisam;
+
+CREATE TABLE IF NOT EXISTS t_innodb(id INT,name VARCHAR(15)) ENGINE = INNODB;
+OPTIMIZE TABLE t_innodb;
 
