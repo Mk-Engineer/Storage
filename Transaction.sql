b8dc358de1e771f48d39a0de644370e940b55526
mk-linux <shiyu0909@outlook.com>
2024-03-28 14:02:55 +0800

    linux update

--- Transaction.sql
+++ Transaction.sql
@@ -55,4 +55,99 @@ SHOW VARIABLES LIKE 'autocommit';
 -- 6 其他语句
 -- 使用ANALYZE TABLE / CACHE INDEX / CHECK TABLE / FLUSH / LOAD INDEX INTO CACHE / OPTIMIZE TABLE / REPAIR TABLE / RESET 等也会触发隐式提交
 
- 
+
+-- 演示代码
+-- 情况1
+CREATE TABLE IF NOT EXISTS users(name VARCHAR(20), PRIMARY KEY(name)) ENGINE=InnoDB; 
+
+BEGIN;
+INSERT INTO users SELECT "ZhangSan";
+COMMIT;
+
+BEGIN;
+INSERT INTO users SELECT 'LiSi';
+INSERT INTO users SELECT 'LiSi';
+ROLLBACK;
+
+SELECT * FROM users;
+
+-- 情况2
+TRUNCATE TABLE users; #DDL操作会自动提交数据，不受autocommit影响
+SELECT * FROM users;
+
+BEGIN;
+INSERT INTO users VALUES('ZHANG SAN');
+COMMIT;
+
+-- 没有BEGIN
+INSERT INTO users VALUES('LI SI');#默认情况下DML操作也会自动提交数据，即autocommit为TRUE时
+INSERT INTO users VALUES('LI SI');
+ROLLBACK;
+
+SELECT * FROM users;
+
+-- 情况3
+TRUNCATE TABLE users;
+SELECT * FROM users;
+
+SELECT @@completion_type;
+SET @@completion_type = 1;
+
+BEGIN;
+INSERT INTO users VALUES('zhangsan');
+COMMIT;
+
+INSERT INTO users VALUES('lisi');
+INSERT INTO users VALUES('lisi');
+ROLLBACK;
+
+SELECT * FROM users;
+
+SET @@completion_type = 0;
+SELECT @@completion_type;
+
+
+-- InnoDB和MyISAM的区别
+SELECT * FROM t_innodb;
+SELECT * FROM t_myisam;
+
+SHOW CREATE TABLE t_innodb;
+SHOW CREATE TABLE t_myisam;
+
+#InnoDB
+BEGIN;
+INSERT INTO t_innodb VALUES(1,'ONE');
+ROLLBACK;
+
+SELECT * FROM t_innodb;
+
+#MyISAM
+-- MyISAM不支持事务
+BEGIN;
+INSERT INTO t_myisam VALUES(2,'TWO');
+ROLLBACK;
+
+SELECT * FROM t_myisam;
+
+#SAVEPOINT
+
+CREATE TABLE IF NOT EXISTS person(name VARCHAR(15),balance DECIMAL(10,2));
+
+BEGIN;
+INSERT INTO person(name,balance) VALUES('ZHANG SAN',3000);
+COMMIT;
+
+SELECT * FROM person;
+
+BEGIN;
+UPDATE person SET balance = balance -100 WHERE NAME = 'ZHANG SAN'; 
+UPDATE person SET balance = balance -100 WHERE NAME = 'ZHANG SAN'; 
+SAVEPOINT s1;#设置保存点
+UPDATE person SET balance = balance +1000 WHERE NAME = 'ZHANG SAN'; 
+ROLLBACK TO s1;#回滚到保存点
+
+SELECT * FROM person;
+
+ROLLBACK;#事务结束
+
+SELECT * FROM person;
