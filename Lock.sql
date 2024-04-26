26512296b41699c22cc523a67a7492ef64a521ea
mk-linux <shiyu0909@outlook.com>
2024-04-26 16:39:58 +0800

    linux update

--- Lock.sql
+++ Lock.sql
@@ -5,11 +5,11 @@ USE dbtest;
 
 -- Tx1
 -- begin
-update employees set salary = salary + 1000 WHERE employee_id = 101;
-update employees set salary = salary - 1000 WHERE employee_id = 102;
+UPDATE employees SET salary = salary + 1000 WHERE employee_id = 101;
+UPDATE employees SET salary = salary - 1000 WHERE employee_id = 102;
 
 -- Tx2
 -- begin
-update employees set salary = salary + 1000 WHERE employee_id = 102;
-update employees set salary = salary - 1000 WHERE employee_id = 101;
+UPDATE employees SET salary = salary + 1000 WHERE employee_id = 102;
+UPDATE employees SET salary = salary - 1000 WHERE employee_id = 101;
 
