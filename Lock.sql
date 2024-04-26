2a0bf8b988c4923d6de1c45816693493bca1c722
mk-linux <shiyu0909@outlook.com>
2024-04-26 17:04:25 +0800

    linux update

--- Lock.sql
+++ Lock.sql
@@ -1,7 +1,7 @@
 #锁
 USE dbtest;
 
-#Dead-Lock
+#死锁
 
 -- Tx1
 -- begin
@@ -13,3 +13,8 @@ UPDATE employees SET salary = salary - 1000 WHERE employee_id = 102;#3
 UPDATE employees SET salary = salary + 1000 WHERE employee_id = 102;#2
 UPDATE employees SET salary = salary - 1000 WHERE employee_id = 101;#4
 
+#解决死锁
+-- 1 超时
+
+-- 2 死锁检测
+--   一旦检测到死锁，InnoDB会选择回滚undo量最小的事务
