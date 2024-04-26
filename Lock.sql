2d0e7c7f3bb600f1d70a25f7dd1089b42883cdde
mk-linux <shiyu0909@outlook.com>
2024-04-26 18:52:43 +0800

    linux update

--- Lock.sql
+++ Lock.sql
@@ -19,4 +19,7 @@ UPDATE employees SET salary = salary - 1000 WHERE employee_id = 101;#4
 -- 2 死锁检测
 --   一旦检测到死锁，InnoDB会选择回滚undo量最小的事务
 
+#锁的监控
+-- 查询当前正在等待的锁
+SELECT * FROM performance_schema.data_lock_waits\G;
 
