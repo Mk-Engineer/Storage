3bb0dfee7617d912342af41e3bfc320554a367d5
Host <shiyu0909@outlook.com>
2024-05-02 21:41:14 +0800

    iPad upload

---
+++ JDBCQuick.java
@@ -0,0 +1,45 @@
+package com.omen.base;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * @author 0xhekt
+ * @date 2024/5/2 - 21:21
+ */
+public class JDBCQuick {
+    public static void main(String[] args) throws Exception {
+        //1.注册驱动
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        //2.获取连接对象
+        String url = "jdbc:mysql://localhost:3306/dbtest";
+        String username = "root";
+        String password = "1111";
+        Connection connection = DriverManager.getConnection(url, username, password);
+
+        //3.获取执行SQL语句的对象
+        Statement statement = connection.createStatement();
+
+        //4.编写SQL语句并执行
+        String sql = "SELECT * FROM t_emp";
+        ResultSet resultSet = statement.executeQuery(sql);//接收返回的结果集
+
+        //5.处理结果，便利resultSet结果集
+        while (resultSet.next()){
+            int empId = resultSet.getInt("emp_id");
+            String empName = resultSet.getString("emp_name");
+            double empSalary = resultSet.getDouble("emp_salary");
+            int empAge = resultSet.getInt("emp_age");
+
+            System.out.println(empId + "\t" + empName + "\t" + empSalary + "\t" + empAge);
+        }
+
+        //6. 释放资源（先卡后关）
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+}
