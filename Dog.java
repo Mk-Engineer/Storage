0704658d98bed17393c5df769fb10618b025aab3
MK-OMEN <shiyu0909@outlook.com>
2024-05-16 16:44:33 +0800

    windows create

---
+++ Dog.java
@@ -0,0 +1,39 @@
+package com.omen.pojo;
+
+import java.util.Objects;
+
+/**
+ * @author 0xhekt
+ * @date 2024/5/16 - 16:16
+ */
+public class Dog {
+    private String name;
+
+    public Dog() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return Objects.equals(name, dog.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
