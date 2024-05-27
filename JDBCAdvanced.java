643ce16a44be93656387cd3ba8c9ae219c14956d
MK-OMEN <shiyu0909@outlook.com>
2024-05-27 11:17:40 +0800

    windows update

--- JDBCAdvanced.java
+++ JDBCAdvanced.java
@@ -87,4 +87,46 @@ public class JDBCAdvanced {
         preparedStatement.close();
         connection.close();
     }
+
+    /*
+    * 主键回显
+    *   在Java程序中获取数据库中插入新数据后的主键值，并赋值给Java对象
+    */
+    @Test
+    public void testReturnPK() throws SQLException {
+        //获取连接
+        Connection connection = DriverManager.getConnection("jdbc:mysql:///dbtest", "root", "1111");
+
+        //预编译SQL语句，告知preparedStatement，返回新增数据的主键列的值
+        String sql = "INSERT INTO t_emp(emp_name,emp_salary,emp_age) VALUES(?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+
+        //创建对象，将对象的属性值，填充在?占位符上(ORM)
+        Employee employee = new Employee(null,"Jack",123.45,29);
+        preparedStatement.setString(1,employee.getEmpName());
+        preparedStatement.setDouble(2,employee.getEmpSalary());
+        preparedStatement.setInt(3,employee.getEmpAge());
+
+        //执行SQL，并获取返回结果
+        int result = preparedStatement.executeUpdate();
+
+        //处理结果
+        if(result > 0){
+            System.out.println("SUCCESS");
+            //获取当前新增数据的主键值，回显到Java中employee对象的empId属性上
+            //返回的主键值是一个单行单列的结果
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            if(resultSet.next()){
+                int empId = resultSet.getInt(1);
+                employee.setEmpId(empId);
+            }
+            System.out.println(employee);
+        } else {
+            System.out.println("FAIL");
+        }
+
+        //释放资源
+        preparedStatement.close();
+        connection.close();
+    }
 }
